93a9b95ee109cb7f3dfeb812515e1955ea36cdae
Nicolas SIMON <nicolas.simon5698@gmail.com>
2023-05-07 16:45:54 +0200

    test: add of some logging on stop_the_count.py

--- stop_the_count.py
+++ stop_the_count.py
@@ -4,11 +4,19 @@
 TO DO
 """
 import logging
+import time
 import tweepy as tw
 import script.counting.count as cc
 import script.utils.connect as uc
 
 LOGGER = logging.getLogger(__name__)
+
+logging.basicConfig(
+    style='{',
+    format='[{asctime}] {levelname} {message}',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.INFO
+)
 
 class MyStream(tw.StreamingClient):
     """
@@ -21,11 +29,16 @@ class MyStream(tw.StreamingClient):
         LOGGER.info('Ready to work')
 
     def on_tweet(self, tweet):
-
+        """
+        TO DO
+        """
+        LOGGER.info('New mission !')
         call_id = api.get_status(tweet.id)
-        tweet_id = api.get_status(call_id.in_reply_to_status_id)
+        tweet_id = api.get_status(call_id.in_reply_to_status_id).id_str
         user_name = 'Pop_Kulture1'
         cc.send_proposals(client, api, user_name, tweet_id, n_proposals=5)
+        time.sleep(0.5)
+
 
 client = uc.get_client()
 api = uc.get_api()
@@ -35,6 +48,7 @@ stream = MyStream(bearer_token=bearer)
 
 active_rules = stream.get_rules().data
 stream.delete_rules(active_rules)
-stream.add_rules(tw.StreamRule("@StopTheCountBot from:Pop_Kulture1"))
+#stream.add_rules(tw.StreamRule("@StopTheCountBot from:Pop_Kulture1"))
+stream.add_rules(tw.StreamRule("@StopTheCountBot from:UnConnaisseur"))
 
 stream.filter(tweet_fields=["referenced_tweets"])
